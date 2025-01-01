72500b79372b7e35e9ea2c469c72f8d2d0d3e5e8
maymichaeli <maushe55@gmail.com>
2025-01-01 21:04:00 +0200

    api+ pix filter and search funcs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,43 @@
 import React from "react";
-import datajson from "../assets/CountriesData.json"
+// import datajson from "../assets/CountriesData.json"
 import { Header } from "../components/Header"
 import { Filters } from "../components/Filters";
 import { CountriesGrid } from "../components/CountriesGrid";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const Home = () => {
-    const [filteredCards, setFilteredCards] = useState(datajson);
+
+    const [originalData, setOriginalData] = useState([]);
+    const [filteredCards, setFilteredCards] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () =>{
+            try {
+                const response = await fetch('https://restcountries.com/v3.1/all')
+                const data = await response.json();
+                console.log(data);
+                const processedData = data.map((country) => ({
+                    name: country.name.common,
+                    population: country.population,
+                    flag: country.flags.png,
+                    region: country.region, 
+                    capital: country.capital
+                }));
+                setOriginalData (processedData);
+                setFilteredCards(processedData);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
+        fetchData();
+    },[])
 
     const handleSearch = (searchValue) => {
-        const filtered = datajson.filter((card) => {
-            const values = Object.values(card).map(value => value.toString().toLowerCase());
+        const filtered = originalData.filter((card) => {
+            const values = Object.values(card)
+            .filter(value=>value!= undefined && value!= null)
+            .map(value => value.toString().toLowerCase());
             return values.some(value => value.includes(searchValue.toLowerCase()));
         });
         setFilteredCards(filtered);
@@ -20,9 +46,9 @@ const Home = () => {
 
     const handleFilter = (region) => {
         if (region === "all") {
-            setFilteredCards(datajson);
+            setFilteredCards(originalData); 
         } else {
-            const filtered = datajson.filter(
+            const filtered = originalData.filter(
                 (card) => card.region.toLowerCase() === region
             );
             setFilteredCards(filtered);
